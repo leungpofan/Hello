365e733b658d6b32363e35e285584060d51ab1f9
leungpofan <leungpofan@hotmail.com>
2015-09-25 23:31:04 +0800

    change source

--- src/Hello.java
+++ src/Hello.java
@@ -1,21 +1,8 @@
 
 public class Hello {
 
-    public static int[] findTwoSum(int[] list, int sum) {
-        for(int i = 0; i < list.length - 1; i++) {
-        	for (int j = 0; j < list.length; j++) {
-        		if (list[i] + list[j] == sum) {
-        			int[] find = new int[] {i, j};
-        			return find;
-        		}
-        	}
-        }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        int[] indices = findTwoSum(new int[] { 1, 3, 5, 7, 9 }, 12);
-        System.out.println(indices[0] + " " + indices[1]);
-    }
+	public static void main(String[] args) {
+		System.out.println("Hello Paul!\n");
+	}
 
 }
