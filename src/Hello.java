8b3bb62ffb1ab9b67245b89e809d8f773c21acc2
leungpofan <leungpofan@hotmail.com>
2015-09-26 21:07:07 +0800

    change line

--- src/Hello.java
+++ src/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello Paul!\n");
 		System.out.println("good!\n");
-		System.out.println("yes!\n");
+		System.out.println("why!\n");
 	}
 
 }
